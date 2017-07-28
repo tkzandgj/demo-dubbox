457b6036a0c5a70f45a240120cc5474223ebd7b7
caoliang <caoliang1918@aliyun.com>
2017-07-28 23:03:53 +0800

    init push

---
+++ dubbox-consumer/src/main/java/com/zhongweixian/DubboxConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.zhongweixian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboxConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboxConsumerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/zhongweixian/DemoDubboxApplication.java
@@ -0,0 +1,12 @@
+package com.zhongweixian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDubboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDubboxApplication.class, args);
+	}
+}
